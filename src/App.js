270efd3f6162359d28169910df33dafc33e2817c
AlinaZakirod <gabie1989@gmail.com>
2019-09-25 05:11:07 -0400

    filtering list added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/movieCell";
+import ListGroup from "./components/common/listGroup";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
+        <ListGroup />
         <Movies />
       </main>
     );
